66be40b80faac532a7ada5d7ba016e9712594682
Hannah Belle <159597144+hannahbellesheart@users.noreply.github.com>
2026-02-04 01:02:05 +0000

    Fix quicklinks and themes pages to work without API calls

---
+++ static/quicklinks-data.js
@@ -0,0 +1,8 @@
+const quicklinksData = [
+  { name: "Open Downloads", description: "Opens the Downloads folder in Finder", link: "~/Downloads" },
+  { name: "Open Documents", description: "Opens the Documents folder in Finder", link: "~/Documents" },
+  { name: "Open Desktop", description: "Opens the Desktop folder in Finder", link: "~/Desktop" },
+  { name: "GitHub", description: "Open GitHub", link: "https://github.com" },
+  { name: "Google", description: "Search on Google", link: "https://google.com" },
+  { name: "YouTube", description: "Open YouTube", link: "https://youtube.com" },
+];
